f2824321e5d3282a01e91c82d8fe815d88b82941
Chems Hoflack <chhoflac@k1r3p8.42mulhouse.fr>
2024-02-14 14:33:17 +0100

    ajout de la fonction reupour checker les bords de map

--- sources/so_long.h
+++ sources/so_long.h
@@ -18,7 +18,7 @@ typedef struct s_elements
 }	t_elements;
 
 int			ft_check_extension(char *f);
-int			ft_check_bordline(char *line);
+int			ft_check_bordlines(char *line);
 int			ft_check_midlines(char *line);
 void		ft_get_elements(char *line, t_elements *elements);
 t_elements	ft_set_struct(int fd);
